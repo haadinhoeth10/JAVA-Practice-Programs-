a69b8df39704a33b8d500e803bda24a617fb476c
haadinhoeth10 <154090128+haadinhoeth10@users.noreply.github.com>
2024-03-04 08:19:53 +0530

    Update FibonacciSeries.java

--- FibonacciSeries.java
+++ FibonacciSeries.java
@@ -1,10 +1,16 @@
+
+ /* Fibonacci Series -> [0 1] 1 2 3 5 8 13 ...
+        [0,1] -> These two numbers are initially fixed in the fibonacci series
+  
+        This program  will take an input from the user, lets say 15, then the fibonacci series displayed will be till 15 only and hence here the last
+        number of our fibonacci series is 13, as it the last number of the fibonacci series less than 15
+        */
+
 import java.util.Scanner;
 
 public class FibonacciSeries {
     public static void main(String[] args) {
-        /* Fibonacci [0 1] 1 2 3 5 8 13 ...
-        enter number 15 fibonacci till 13 only last number less than 15
-        */
+       
         Scanner input =new Scanner(System.in);
         System.out.println("Welcome to fibonacci calculator");
         System.out.println("Enter no till where fibonacci is to be calculated");
@@ -12,9 +18,12 @@ public class FibonacciSeries {
         fibonacciNumber(number);
     }
     public static void fibonacciNumber(int num1){
-        //constant
+
         if(num1 <0) return;
         if(num1 == 0) return;
+
+        //constant
+
 
 
         System.out.print(0 + " ");
